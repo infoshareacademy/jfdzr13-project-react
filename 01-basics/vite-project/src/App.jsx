a251e57ae68c61f4d1a3bdc97fd673510751a419
Klaudia Gajewska <gajewskaklaudia99@gmail.com>
2024-03-17 10:11:54 +0100

    added plan.txt, edited app.jsx

--- 01-basics/vite-project/src/App.jsx
+++ 01-basics/vite-project/src/App.jsx
@@ -1,16 +1,40 @@
-import React from 'react'
+//react
+import React from 'react';
+
+//components
+import HeaderWithButton from './components/HeaderWithButton'
+
+//styles
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <div className='containter'>
-        <p>menu</p>
-        <div className='wrapper'>
-
+    <div className='main'>
+      <div className='navigation-bar'>
+        <logo/>
+        <menu/>
+      </div>
+      <div className='main-content'>
+        <div className='main-article'>
+          <main-image/>
+          <big-header/>
+          <description/>
+        </div>
+        <div className='new-artivle'>
+          <title/>
+          <new-article-1/>
+          <new-article-2/>
+          <new-article-3/>
+        </div>
+        <div className='top-3-articles'>
+          <top-article-1/>
+          <top-article-2/>
+          <top-article-3/>
         </div>
       </div>
+    </div>
     </>
   )
 }

2679fc3da591324ad71eaba7eddaec8077ded32f
EMILIA <emilia.bechta2912@gmail.com>
2024-04-26 08:46:50 +0200

    Add dictionary types

--- 04-typescript/dictionary/src/context/DictionaryContext.tsx
+++ 04-typescript/dictionary/src/context/DictionaryContext.tsx
@@ -6,48 +6,57 @@ const BASE_URL = "https://api.dictionaryapi.dev/api/v2/entries/en";
 export const DictionaryContext = React.createContext();
 
 export const useDictionary = () => {
-	return useContext(DictionaryContext);
+  return useContext(DictionaryContext);
 };
 
-// Otypować definition:
+type State = "idle" | "pending" | "resolved" | "rejected";
+
+type DefinitionItem = {
+  definition: string;
+  synonyms: string[];
+  antonyms: string[];
+  example: string;
+};
+
+type Definition = {
+  partOfSpeech: "noun" | "verb" | "adjective" | "adverb" | "pronoun";
+  definitions: DefinitionItem[];
+};
+
 type DictionaryType = {
-	loadingState: "idle" | "pending" | "resolved" | "rejected";
-	definition:
-		| []
-		| Array<{
-				partOfSpeech: "noun" | "verb" | "adjective" | "adverb" | "pronoun";
-				antonyms: unknown[];
-		  }>;
+  loadingState: State;
+  definition: Definition[];
 };
 
 export const DictionaryProvider = ({ children }: { children: JSX.Element }) => {
-	const [data, setData] = useState<DictionaryType>({
-		definition: [],
-		loadingState: "idle",
-	});
+  const [data, setData] = useState<DictionaryType>({
+    definition: [],
+    loadingState: "idle",
+  });
+  console.log({ data });
 
-	const getDefinition = async (word: string) => {
-		setData((prev) => ({ ...prev, loadingState: "pending" }));
-		await fetch(`${BASE_URL}/${word}`)
-			.then((res) => res.json())
-			.then((fetchedDefinition) =>
-				setData((prev) => ({
-					...prev,
-					definition: fetchedDefinition[0].meanings,
-					loadingState: "resolved",
-				}))
-			)
-			.catch((error) => {
-				console.error(error);
-				setData((prev) => ({ ...prev, loadingState: "rejected" }));
-			});
-	};
+  const getDefinition = async (word: string) => {
+    setData((prev) => ({ ...prev, loadingState: "pending" }));
+    await fetch(`${BASE_URL}/${word}`)
+      .then((res) => res.json())
+      .then((fetchedDefinition) =>
+        setData((prev) => ({
+          ...prev,
+          definition: fetchedDefinition[0].meanings,
+          loadingState: "resolved",
+        }))
+      )
+      .catch((error) => {
+        console.error(error);
+        setData((prev) => ({ ...prev, loadingState: "rejected" }));
+      });
+  };
 
-	const passedData = { data, getDefinition };
+  const passedData = { data, getDefinition };
 
-	return (
-		<DictionaryContext.Provider value={passedData}>
-			{children}
-		</DictionaryContext.Provider>
-	);
+  return (
+    <DictionaryContext.Provider value={passedData}>
+      {children}
+    </DictionaryContext.Provider>
+  );
 };
